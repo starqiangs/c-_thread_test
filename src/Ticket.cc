d0ec94fc430317e460acfb62fe3dfb441214362e
qiang <star.lvqiang@gmail.com>
2023-08-22 15:39:42 +0800

    add lock guard replace mutex

--- src/Ticket.cc
+++ src/Ticket.cc
@@ -9,13 +9,16 @@ void Ticket::SellTicket(int id)
 {
     while (this->ticket_count > 0)
     {
-        mtx.lock();
-        if (this->ticket_count > 0)
         {
-            std::cout << "window:" << id << " sell the number:" << ticket_count << "ticket" << std::endl;
-            this->ticket_count--;
+            // mtx.lock();
+            std::lock_guard<std::mutex> lock(this->mtx);
+            if (this->ticket_count > 0)
+            {
+                std::cout << "window:" << id << " sell the number:" << ticket_count << "ticket" << std::endl;
+                this->ticket_count--;
+            }
+            // mtx.unlock();
         }
-        mtx.unlock();
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
